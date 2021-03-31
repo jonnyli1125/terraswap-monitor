92f565cd81f2f70a8330aa4a8544055e73ca9d52
Jonny Li <myself@jonny.li>
2021-03-30 23:50:27 -0700

    Add Luna/UST swap rate, simple bot commands

--- bot.py
+++ bot.py
@@ -1,20 +1,21 @@
+#!/usr/bin/env python3
 import asyncio
-import discord
+import json
+import discord.ext.commands
 from terraswap import TerraswapMonitor
 
-
-class TerraswapMonitorBot(discord.Client):
+class TerraswapMonitorBot(discord.ext.commands.Bot):
     def __init__(self, config):
         self.config = config['discord']
         self.ts = TerraswapMonitor(config['terraswap'])
         self.rates = {}
-        for pair in self.config['pairs']:
-            self.rates[pair] = [float('inf'), float('inf')]
         self.thresholds = self.config['thresholds']
+        for pair in self.thresholds:
+            self.rates[pair] = [(float('inf'), float('inf'))]
         self.interval = self.config['interval']
         self.log_channel = None
         self.ping_list = self.config['ping_list']
-        super().__init__()
+        super().__init__(command_prefix='!')
 
     def run(self):
         super().run(self.config['login_token'])
@@ -26,19 +27,55 @@ class TerraswapMonitorBot(discord.Client):
         await self.monitor()
 
     async def monitor(self):
+        # TODO: use discord.ext.tasks eventually
         while True:
             luna_to_bluna = await self.ts.get_luna_to_bluna_rate()
             bluna_to_luna = await self.ts.get_bluna_to_luna_rate()
+            luna_to_ust = await self.ts.get_luna_to_ust_rate()
             rates = self.rates['bluna_luna']
             threshold = self.thresholds['bluna_luna']
             msg = (
 				"```" +
 				"Luna to bLUNA - {} bLUNA per Luna\n" +
-				"bLUNA to Luna - {} bLUNA per Luna" +
-				"```").format(luna_to_bluna, bluna_to_luna)
-            if (luna_to_bluna > threshold[0] and rates[0] < threshold[0]) or (bluna_to_luna > threshold[1] and rates[1] < threshold[1]):
+				"bLUNA to Luna - {} bLUNA per Luna\n" +
+                "Luna to UST   - {} UST per Luna" +
+				"```").format(luna_to_bluna, bluna_to_luna, luna_to_ust)
+            # TODO: generalize this logic eventually
+            if (luna_to_bluna > threshold and rates[0] < threshold) or (bluna_to_luna > threshold and rates[1] < threshold):
                 for uid in self.ping_list:
                     msg += "<@{}> ".format(uid)
             self.rates['bluna_luna'] = [luna_to_bluna, bluna_to_luna]
             await self.log_channel.send(msg)
             await asyncio.sleep(self.interval)
+
+
+config = {}
+with open('config.json', 'r') as fp:
+    config = json.load(fp)
+bot = TerraswapMonitorBot(config)
+
+@bot.command()
+async def interval(ctx, val:int=None):
+    if not val:
+        await ctx.send("Current interval: {} seconds".format(bot.interval))
+        return
+    bot.interval = val
+    await ctx.send("Updated interval to {} seconds.".format(val))
+
+@bot.command()
+async def threshold(ctx, pair:str=None, val:float=None):
+    if not pair:
+        pairs = ', '.join(['`{}`'.format(p) for p in bot.thresholds])
+        await ctx.send("Current pairs with thresholds: {}".format(pairs))
+        return
+    if not val:
+        await ctx.send("Current threshold of pair `{}`: {}".format(pair, bot.thresholds[pair]))
+        return
+    bot.thresholds[pair] = val
+    await ctx.send("Updated threshold of pair `{}` to {}.".format(pair, val))
+
+if __name__ == '__main__':
+    try:
+        bot.run()
+    except KeyboardInterrupt:
+        print('Exiting...')
--- terraswap.py
+++ terraswap.py
@@ -7,30 +7,29 @@ class TerraswapMonitor:
         self.pair_addresses = config["pairs"]
         self._simulation_input_amt = 1000000
 
-    async def get_luna_to_bluna_rate(self):
-        '''Return swap rate of Luna to bLUNA transaction in units of bLUNA per Luna.'''
+    async def get_swap_rate(self, pair, reverse=False, input_denom="uluna"):
+        '''Return swap rate of input_denom to other token in pair, in units of other token per input_denom.'''
         data = {
-            "simulation": {
-                "offer_asset": {
-                    "info": {"native_token": {"denom": "uluna"}},
+            ("reverse_simulation" if reverse else "simulation"): {
+                ("ask_asset" if reverse else "offer_asset"): {
+                    "info": {"native_token": {"denom": input_denom}},
                     "amount": str(self._simulation_input_amt)
                 }
             }
         }
-        res = await self.terra.wasm.contract_query(self.pair_addresses["bluna_luna"], data)
-        bluna_amt = res['return_amount']
-        return int(bluna_amt) / self._simulation_input_amt
+        pair_addr = self.pair_addresses[pair]
+        res = await self.terra.wasm.contract_query(pair_addr, data)
+        other_amt = res['offer_amount' if reverse else 'return_amount']
+        return int(other_amt) / self._simulation_input_amt
+
+    async def get_luna_to_bluna_rate(self):
+        '''Return swap rate of Luna to bLUNA transaction in units of bLUNA per Luna.'''
+        return await self.get_swap_rate("bluna_luna")
 
     async def get_bluna_to_luna_rate(self):
         '''Return swap rate of bLUNA to Luna transaction in units of bLUNA per Luna.'''
-        data = {
-            "reverse_simulation": {
-                "ask_asset": {
-                    "info": {"native_token": {"denom": "uluna"}},
-                    "amount": str(self._simulation_input_amt)
-                }
-            }
-        }
-        res = await self.terra.wasm.contract_query(self.pair_addresses["bluna_luna"], data)
-        bluna_amt = res['offer_amount']
-        return int(bluna_amt) / self._simulation_input_amt
+        return await self.get_swap_rate("bluna_luna", reverse=True)
+
+    async def get_luna_to_ust_rate(self):
+        '''Return swap rate of Luna to UST transaction in units of UST per Luna'''
+        return await self.get_swap_rate("ust_luna")
